98d1321112dad8e7a6d0fe039edea4ee735d234e
wcampos <wil@valuestreamengineers.com>
2025-03-15 20:09:37 -0500

    feat: fix gha errord

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "cloudwatch_event_rule_arn" {
   description = "ARN of the CloudWatch Event rule"
-  value       = aws_cloudwatch_event_rule.lambda_warmer_cw_event_rule.arn
+  value       = try(aws_cloudwatch_event_rule.lambda_warmer_cw_event_rule[0].arn, "")
 }
 
 output "cloudwatch_event_rule_name" {
   description = "Name of the CloudWatch Event rule"
-  value       = aws_cloudwatch_event_rule.lambda_warmer_cw_event_rule.name
+  value       = try(aws_cloudwatch_event_rule.lambda_warmer_cw_event_rule[0].name, "")
 }
 
 output "lambda_permission_id" {
   description = "ID of the Lambda permission"
-  value       = aws_lambda_permission.lambda_warmer_invoke_permissions.id
+  value       = try(aws_lambda_permission.lambda_warmer_invoke_permissions[0].id, "")
 } 
